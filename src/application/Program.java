5aed810bf173970597cb7a610c4e035ce5094e7e
lucas <farias.maciel@outlook.com>
2021-02-14 01:29:15 -0400

    Add classs Board and Piece

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,15 @@
 package application;
 
+import BoardGame.Board;
 import BoardGame.Position;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Position pos = new Position(2, 4);
-		System.out.println(pos);
+		Board board = new Board(2, 8);
+		System.out.println(board);
+		
+		
 	}
 
 }
